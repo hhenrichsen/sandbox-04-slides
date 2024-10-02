169064e3b010d8db2963ef561e8e5790fc13f24c
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-10-01 22:03:59 -0600

    :recycle: Add entrypoints to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import {defineConfig} from 'vite';
 import UnoCSS from 'unocss/vite';
+import {resolve} from 'path';
 
 export default defineConfig({
   base: process.env.REPO_NAME ? `/${process.env.REPO_NAME}/` : '/',
   plugins: [UnoCSS()],
+  build: {
+    rollupOptions: {
+      input: {
+        '01': resolve(__dirname, '01.html'),
+        '02': resolve(__dirname, '02.html'),
+        '03': resolve(__dirname, '03.html'),
+        '04': resolve(__dirname, '04.html'),
+      },
+    },
+  },
 });
