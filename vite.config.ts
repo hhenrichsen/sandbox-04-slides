ec493157c220241d5d0bebe4a5c3c55c08233a49
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2024-10-16 15:11:49 -0600

    :sparkles: Add lecture 6 slides

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
         '02': resolve(__dirname, '02.html'),
         '03': resolve(__dirname, '03.html'),
         '04': resolve(__dirname, '04.html'),
+        '05': resolve(__dirname, '05.html'),
       },
     },
   },
